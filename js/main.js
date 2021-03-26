3a5cf9fa473d1f8142b4cf727f9586f856aaf702
Artem Udovichenko <artemudovichenko90@gmail.com>
2021-03-26 16:32:00 +0200

    Create array of users and employees

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const users = [
+    new User('Pavel Pavlovich', 40, 'man', 'pavlovich@gmail.com', 'Russia'),
+    new User('Rita Naumovna', 77, 'female', 'naumovna@exmaple.com.com', 'Ukraine'),
+    new User('Fedr Fedorovich', 45, 'man', 'fedorovich@gmail.com', 'Ukraine'),
+    new Employee('Ivan Ivanov', 22, 'man', 'ivanov@gmail.com', 'Ukraine', 1000, 'junior', 'developing'),
+    new Employee('Petr Petrov', 24, 'man', 'petrov@exmaple.com', 'Russia', 1100, 'junior', 'developing'),
+    new Employee('Stepan Stepanovich', 16, 'man', 'stepanovich@gmail.com', 'Ukraine', 1200, 'junior', 'developing'),
+    new Employee('Oleg Olegovich', 17, 'man', 'olegovich@exmaple.com', 'Russia', 1100, 'junior', 'developing'),
+    new Employee('Danil', 30, 'man', 'danilovich@gmail.com', 'Ukraine', 1000, 'junior', 'developing'),
+    new Employee('Olga Andreeva', 15, 'female', 'andreeva@gmail.com', 'Ukraine', 500, 'junior', 'design'),
+    new Employee('Svetlana Sergeeva', 19, 'female', 'sergeeva@exmaple.com', 'Ukraine', 700, 'junior', 'design')
+];
