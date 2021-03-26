781b0b34a8c7d6066e16557536082bd59fd1a110
Artem Udovichenko <artemudovichenko90@gmail.com>
2021-03-26 16:29:21 +0200

    Add class User

---
+++ js/user.js
@@ -0,0 +1,93 @@
+class User {
+    #name;
+    #age;
+    #male;
+    #email;
+    #country;
+
+    constructor(name, age, male, email, country) {
+        this.#name = name;
+        this.#age = age;
+        this.#male = male;
+        this.#email = email;
+        this.#country = country;
+    }
+    get name() {
+        return this.#name;
+    }
+
+    set name(value) {
+        this.#name = value;
+    }
+
+    get age() {
+        return this.#age;
+    }
+
+    set age(value) {
+        this.#age = value;
+    }
+
+    get male() {
+        return this.#male;
+    }
+
+    set male(value) {
+        this.#male = value;
+    }
+
+    get email() {
+        return this.#email;
+    }
+
+    set email(value) {
+        this.#email = value;
+    }
+
+    get country() {
+        return this.#country;
+    }
+
+    set country(value) {
+        this.#country = value;
+    }
+    get name() {
+        return this.#name;
+    }
+
+    set name(value) {
+        this.#name = value;
+    }
+
+    get age() {
+        return this.#age;
+    }
+
+    set age(value) {
+        this.#age = value;
+    }
+
+    get male() {
+        return this.#male;
+    }
+
+    set male(value) {
+        this.#male = value;
+    }
+
+    get email() {
+        return this.#email;
+    }
+
+    set email(value) {
+        this.#email = value;
+    }
+
+    get country() {
+        return this.#country;
+    }
+
+    set country(value) {
+        this.#country = value;
+    }
+}
